ad6851b0aa8eaa172ea12da07d054ac60f5a59db
Nate Gorham <nathanielgorham00@gmail.com>
2024-08-15 14:41:15 -0400

    created ToDo class

---
+++ ToDo.java
@@ -0,0 +1,13 @@
+package ToDoApp;
+
+import java.util.Scanner;
+
+public class ToDo 
+{
+    public static void main(String[] args) {
+        ToDoList todo = new ToDoList();
+        Scanner kbd = new Scanner(System.in);
+
+        todo.start(kbd);
+    }
+}
