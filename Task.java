4f4c1fbe19f3e7f7a42cf419b02ecc1420aca3cc
Nate Gorham <nathanielgorham00@gmail.com>
2024-08-15 13:48:06 -0400

    created task object

---
+++ Task.java
@@ -0,0 +1,40 @@
+package ToDoApp;
+
+public class Task {
+    private String task;
+    private boolean status;
+
+    public Task() {
+        task = "";
+        status = false;
+    }
+
+    public Task(String task) {
+        this.task = task;
+        status = false;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void update() {
+        status = true;
+    }
+
+    public String toString() {
+        return String.format("%s | completed: %B\n", task, status);
+    }
+}
